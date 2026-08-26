2dbfae70399b83b98bb5842898c755898752b728
MSc Bioinformatics <logancollitt2009@gmail.com>
2026-08-26 18:19:00 +0100

    Create Dplyr.R

---
+++ Dplyr.R
@@ -0,0 +1,17 @@
+############################################################
+# Function:Descriptives
+############################################################  # Median
+
+median <-meta_info %>%
+  group_by(Disease_status)%>% 
+  summarise(median = median(Age))
+print(median)
+
+###########################################################  # Mean
+
+mean_age <-meta_info %>%
+  group_by(Disease_status)%>% 
+  summarise(average_age = mean(Age))
+print(mean_age)
+
+############################################################  # Min
